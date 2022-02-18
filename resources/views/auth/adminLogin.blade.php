da6db81141ef0ae64c448a48e27057dd10333220
Dhika[backend] <sgibdd1@gmail.com>
2022-02-18 16:35:50 +0700

    perbaikan tampilan

--- resources/views/auth/adminLogin.blade.php
+++ resources/views/auth/adminLogin.blade.php
@@ -1,40 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Neumorphism/Soft UI</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <style type="text/css">
-            .form-control,
-            .form-control:focus,
-            .btn {
-                border-radius: 50px;
-                background: #e0e0e0;
-                box-shadow: 20px 20px 60px #bebebe,
-                    -20px -20px 60px #ffffff;
-            }
-        </style>
+        <title>Administrator Menu</title>
+        @include('includes.script')
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-6 offset-3">
-                    <h1 class="text-center m-5">Admin login</h1>
+                <div class="col-4 offset-3">
+                    <figure class="text-center m-5">
+                    <p class="h3">Administrator Login</p>
+                    </figure>
                     @if(\Session::has('error'))
                         <div class="alert alert-danger">{{ \Session::get('error') }}</div>
                     @endif
                     <form method="post" action="{{ route('admin.login') }}">
                         @csrf
-                        <div class="row mt-3">
-                                <div class="form-floating mb-3">
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="Input email address">
-                                    <label for="email">Email address</label>
+                                <div class="row mt-3">
+                                    <input type="email" style="border-radius: 50px" class="form-control" id="email" name="email" placeholder="Input email address">
                                 </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" id="password" name="password" placeholder="Input password">
-                                    <label for="password">Password</label>
-                                <button type="submit" class="btn">Submit</button>
-                            </div>
-                        </div>
+                                <div class="row mt-3">
+                                    <input type="password" style="border-radius: 50px" class="form-control" id="password" name="password" placeholder="Input password">
+                                </div>
+                                <div class="row mt-3">
+                                <button type="submit" class="btn btn-primary" style="border-radius: 50px">Submit</button>
+                                </div>
                     </form>
                 </div>
             </div>
